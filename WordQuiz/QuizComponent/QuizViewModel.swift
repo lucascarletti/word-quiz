72be19b4a48a452d5f845dfa19f2853ab83e8211
Lucas Teixeira Carletti <lucas.carletti@acad.pucrs.br>
2019-09-16 19:05:10 -0300

    QuizViewModel's final refactors and better name usage

--- WordQuiz/QuizComponent/QuizViewModel.swift
+++ WordQuiz/QuizComponent/QuizViewModel.swift
@@ -15,19 +15,17 @@ struct AlertMessageModel {
 
 class QuizViewModel {
     
-    var timer: QuizTimer?
-    
-    var onQuizRefreshed: ((Quiz) -> Void)?
-    var onRestartQuiz: (() -> Void)?
-    var onHittenWord: ((String, [String]?) -> Void)?
+    /// when quiz is received from API
+    var onInformationChanged: ((String?, [String]) -> Void)?
+    var onInformationFailed: ((AlertMessageModel) -> Void)?
+
+    var onHittenWord: (([String], [String]) -> Void)?
 
     var onSuccessFinish: ((AlertMessageModel) -> Void)?
     var onFailureFinish: ((AlertMessageModel) -> Void)?
-    
+
     var onQuizStart: (() -> Void)?
-    var onQuizStopped: (() -> Void)?
-    
-    var onInformationFailed: ((String?) -> Void)?
+    var onQuizStopped: (([String]) -> Void)?
 
     private(set) var state: QuizState? {
         didSet {
@@ -36,116 +34,102 @@ class QuizViewModel {
         }
     }
 
-    private(set) var wordsDataSource: [String]? = {
-        return [String]()
-    }()
+    private(set) var wordsDataSource = [String]()
     
     private(set) var hittenWords = [String]()
     
+    private let service: QuizServiceProtocol
+    
+    init(service: QuizServiceProtocol = QuizService()) {
+        self.service = service
+    }
+    
+    // MARK : - Public methods
     func initialize() {
-        getQuiz { [weak self] quiz, errorMessage in
-            guard let quiz = quiz, let answer = quiz.answer else {
-                return
+        // NOTE: Current index has default value 1 as we only have a single quiz on our API
+        let endpoint = QuizEndPoint.questions(ofIndex: 1)
+        service.getQuestions(endpoint: endpoint) { [weak self] quiz, errorMessage in
+            if let errorMessage = errorMessage {
+                self?.onInformationFailed?(AlertMessageModel(title: "Ops",
+                                                             message: errorMessage,
+                                                             buttonTitle: "Try Again"))
+            } else if let quiz = quiz, let answer = quiz.answer {
+                self?.wordsDataSource = answer
+                self?.onInformationChanged?(quiz.question, answer)
+                //todo: on success, remove loader spinning, update tableView and let user start
+            } else {
+                self?.onInformationFailed?(AlertMessageModel(title: "Ops",
+                                                             message: "Something went wrong",
+                                                             buttonTitle: "Try Again"))
             }
-            
-            self?.wordsDataSource = answer
-            self?.onQuizRefreshed?(quiz)
-            //todo: on success, remove loader spinning, update tableView and let user start
         }
     }
     
-    func didUpdateTextFieldText(withText text: String) {
-        let hasHittenWord = self.hasHittenWord(withText: text)
+    func didUpdateTextField(withText text: String) {
+        let hasHitten = hasHittenWord(withText: text)
         
-        if hasHittenWord && !hittenWords.contains(text) {
-            hittenWords.append(text)
-            onHittenWord?(text, wordsDataSource)
+        if hasHitten && !hittenWords.contains(text) {
+            hittenWords.insert(text, at: 0)
+            onHittenWord?(hittenWords, wordsDataSource)
             validateQuizCompletion()
         }
     }
     
     func shouldAllowTextFieldReplacementString(fromText text: String) -> Bool {
-        let hasHittenWord = self.hasHittenWord(withText: text)
-        if hasHittenWord && !hittenWords.contains(text) {
+        let hasHitten = hasHittenWord(withText: text)
+        if hasHitten && !hittenWords.contains(text) {
             return false
         }
         
         return true
     }
     
-    func hasHittenWord(withText text: String) -> Bool {
-        return wordsDataSource?.contains(text) ?? false
-    }
-    
-    func quizStateUpdated(newState state: QuizState) {
-        switch state {
-        case .runing:
-            onQuizStart?()
-        
-        case .stopped:
-            hittenWords.removeAll()
-            onQuizStopped?()
-        }
-    }
-    
     func shouldChangeQuizState() {
         if let state = state {
             switch state {
-            case .runing:
+            case .running:
                 self.state = .stopped
             case .stopped:
-                self.state = .runing
+                self.state = .running
             }
         } else {
-            //this else case means that, state has not yet initialized, as it starts in a "stopped" state, we can start runing quiz once it's nil and hitted
-            self.state = .runing
-        }
-    }
-    
-    func validateQuizCompletion() {
-        if hittenWords.count == wordsDataSource?.count {
-            let alertModel = AlertMessageModel(title: "Congratulations",
-                                               message: "Good job! You found all the answers on time. Keep up with great work.",
-                                               buttonTitle: "Play Again")
-            onSuccessFinish?(alertModel)
+            //this else case means that, state has not yet initialized, as it starts in a "stopped" state, we can start running quiz once it's nil and hitted
+            self.state = .running
         }
     }
     
     func timerDidEnd() {
         let hitten = hittenWords.count
-        let total = wordsDataSource?.count ?? 0
+        let total = wordsDataSource.count
         
         let alertModel = AlertMessageModel(title: "Time finished!",
                                            message: "Sorry! Time is up. You got \(hitten) out of \(total)",
-                                           buttonTitle: "Try Again")
-        onSuccessFinish?(alertModel)
+            buttonTitle: "Try Again")
+        onFailureFinish?(alertModel)
     }
-}
-
-//MARK: - Network Management
-extension QuizViewModel {
-    // MARK: - Request Quiz from API
-    // current index has default value 1 as we only have a single quiz on our API
-    func getQuiz(ofIndex index: Int = 1, completion: @escaping (_ quiz: Quiz?, _ error: String?) -> ()) {
-        QuizEndPoint.router.request(.getQuestion(ofIndex: index)) { data, response, error in
-            if error != nil {
-                completion(nil, NetworkResponse.connectionError.rawValue)
-            } else if let httpResponse = response as? HTTPURLResponse, let data = data {
-                let result = NetworkManager.handleNetworkResponse(httpResponse)
-                switch result {
-                case .success:
-                    do {
-                        let quiz = try JSONDecoder().decode(Quiz.self, from: data)
-                        completion(quiz, nil)
-                    } catch {
-                        completion(nil, NetworkResponse.unableToDecode.rawValue)
-                    }
-                case .failure(let errorMessage):
-                    completion(nil, errorMessage)
-                }
-            } else {
-                completion(nil, NetworkResponse.noData.rawValue)
-            }
+    
+    // MARK: - Private methods
+    private func hasHittenWord(withText text: String) -> Bool {
+        return wordsDataSource.contains(text)
+    }
+    
+    private func quizStateUpdated(newState state: QuizState) {
+        switch state {
+        case .running:
+            onQuizStart?()
+            
+        case .stopped:
+            hittenWords.removeAll()
+            onQuizStopped?(wordsDataSource)
+        }
+    }
+    
+    private func validateQuizCompletion() {
+        if hittenWords.count == wordsDataSource.count, !wordsDataSource.isEmpty {
+            let alertModel = AlertMessageModel(title: "Congratulations",
+                                               message: "Good job! You found all the answers on time. Keep up with great work.",
+                                               buttonTitle: "Play Again")
+            onSuccessFinish?(alertModel)
         }
     }
 }
